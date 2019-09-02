ba46309d940dd71126c5df0147cbebeaa910d085
jim chen <chenzemingczm@163.com>
2019-09-02 15:05:23 +0800

    添加对象管理器及单元测试

---
+++ ObjectMgr.h
@@ -0,0 +1,286 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 jewmin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef Foundation_ObjectMgr_INCLUDED
+#define Foundation_ObjectMgr_INCLUDED
+
+#include "CommonDef.h"
+
+namespace Foundation {
+	template<class OBJECT>
+	class ObjectMgr {
+		struct CHUNK {
+			u32 object_count;
+			OBJECT * * objects;
+		};
+
+	public:
+		ObjectMgr(u32 objectCount, u32 chunkCount);
+		~ObjectMgr();
+
+		u32 GetObjCount();
+		OBJECT * GetObj(u32 id);
+		u32 AddNewObj(OBJECT * object);
+		u32 AddNewObj(u32 id, OBJECT * object);
+		OBJECT * RemoveObj(u32 id);
+		bool DeleteObj(u32 id);
+		void DeleteAllObjs();
+		void EnumEachObj(void(*EachFunc)(void * object, void * ud), void * ud);
+
+	protected:
+		CHUNK * AllocOneChunk();
+
+	private:
+		u32 chunk_mod_;
+		u32 object_mod_;
+		u32 object_count_per_chunk_;
+		u32 init_chunk_count_;
+		u32 next_chunk_index_;
+		u32 next_object_index_;
+		u32 next_object_id_;
+		std::vector<CHUNK *> active_chunk_;
+		std::list<CHUNK *> free_chunk_;
+	};
+}
+
+template<class OBJECT>
+Foundation::ObjectMgr<OBJECT>::ObjectMgr(u32 objectCount, u32 chunkCount)
+	: init_chunk_count_(chunkCount), next_chunk_index_(0), next_object_index_(1), next_object_id_(1) {
+	if (objectCount > 8) {
+		if (objectCount < 4096) {
+			chunk_mod_ = 0;
+			object_count_per_chunk_ = 1;
+			--objectCount;
+			while (objectCount > 0) {
+				objectCount >>= 1;
+				object_count_per_chunk_ <<= 1;
+				++chunk_mod_;
+			}
+		} else {
+			object_count_per_chunk_ = 4096;
+			chunk_mod_ = 12;
+		}
+	} else {
+		object_count_per_chunk_ = 8;
+		chunk_mod_ = 3;
+	}
+	object_mod_ = object_count_per_chunk_ - 1;
+	active_chunk_.reserve(init_chunk_count_);
+}
+
+template<class OBJECT>
+Foundation::ObjectMgr<OBJECT>::~ObjectMgr() {
+	DeleteAllObjs();
+}
+
+template<class OBJECT>
+typename Foundation::ObjectMgr<OBJECT>::CHUNK * Foundation::ObjectMgr<OBJECT>::AllocOneChunk() {
+	CHUNK * chunk = nullptr;
+	if (!free_chunk_.empty()) {
+		chunk = free_chunk_.back();
+		free_chunk_.pop_back();
+	} else {
+		chunk = new CHUNK();
+		chunk->objects = static_cast<OBJECT * *>(malloc(sizeof(OBJECT *) * object_count_per_chunk_));
+		chunk->object_count = 0;
+		for (u32 i = 0; i < object_count_per_chunk_; ++i) {
+			*(chunk->objects + i) = nullptr;
+		}
+	}
+	return chunk;
+}
+
+template<class OBJECT>
+u32 Foundation::ObjectMgr<OBJECT>::GetObjCount() {
+	u32 count = 0;
+	for (auto & it : active_chunk_) {
+		CHUNK * chunk = it;
+		if (chunk && chunk->object_count > 0) {
+			count += chunk->object_count;
+		}
+	}
+	return count;
+}
+
+template<class OBJECT>
+OBJECT * Foundation::ObjectMgr<OBJECT>::GetObj(u32 id) {
+	if (0 == id || id >= next_object_id_) {
+		return nullptr;
+	}
+	u32 chunk_index = id >> chunk_mod_;
+	u32 object_index = id & object_mod_;
+	CHUNK * chunk = active_chunk_[chunk_index];
+	if (chunk) {
+		return *(chunk->objects + object_index);
+	}
+	return nullptr;
+}
+
+template<class OBJECT>
+u32 Foundation::ObjectMgr<OBJECT>::AddNewObj(OBJECT * object) {
+	if (!object) {
+		throw std::invalid_argument("object is nullptr");
+	}
+	CHUNK * chunk = nullptr;
+	if (next_chunk_index_ == active_chunk_.size()) {
+		chunk = AllocOneChunk();
+		active_chunk_.push_back(chunk);
+	} else {
+		chunk = active_chunk_.back();
+	}
+	*(chunk->objects + next_object_index_) = object;
+	++chunk->object_count;
+	if (++next_object_index_ == object_count_per_chunk_) {
+		next_object_index_ = 0;
+		++next_chunk_index_;
+	}
+	return next_object_id_++;
+}
+
+template<class OBJECT>
+u32 Foundation::ObjectMgr<OBJECT>::AddNewObj(u32 id, OBJECT * object) {
+	if (0 == id) {
+		throw std::out_of_range("0 < id");
+	} else if (!object) {
+		throw std::invalid_argument("object is nullptr");
+	}
+	if (id >= next_object_id_) {
+		next_object_id_ = id + 1;
+	}
+	u32 chunk_index = id >> chunk_mod_;
+	u32 object_index = id & object_mod_;
+	if (chunk_index >= active_chunk_.size()) {
+		for (u32 i = static_cast<u32>(active_chunk_.size()); chunk_index >= i; ++i) {
+			active_chunk_.push_back(AllocOneChunk());
+		}
+	}
+	if (next_chunk_index_ < chunk_index) {
+		next_chunk_index_ = chunk_index;
+	}
+	if (next_chunk_index_ == chunk_index) {
+		if (object_index + 1 == object_count_per_chunk_) {
+			next_object_index_ = 0;
+			++next_chunk_index_;
+		} else if (object_index >= next_object_index_) {
+			next_object_index_ = object_index + 1;
+		}
+	}
+	CHUNK * chunk = active_chunk_[chunk_index];
+	if (!chunk) {
+		chunk = AllocOneChunk();
+		active_chunk_[chunk_index] = chunk;
+	}
+	if (*(chunk->objects + object_index)) {
+		throw std::invalid_argument("id position has object");
+	}
+	*(chunk->objects + object_index) = object;
+	++chunk->object_count;
+	return id;
+}
+
+template<class OBJECT>
+OBJECT * Foundation::ObjectMgr<OBJECT>::RemoveObj(u32 id) {
+	if (0 == id || id >= next_object_id_) {
+		throw std::out_of_range("0 < id < next_object_id_");
+	}
+	u32 chunk_index = id >> chunk_mod_;
+	u32 object_index = id & object_mod_;
+	CHUNK * chunk = active_chunk_[chunk_index];
+	if (!chunk) {
+		return nullptr;
+	}
+	OBJECT * object = *(chunk->objects + object_index);
+	if (object) {
+		*(chunk->objects + object_index) = nullptr;
+		if (--chunk->object_count == 0) {
+			if (next_chunk_index_ != chunk_index) {
+				free_chunk_.push_back(chunk);
+				active_chunk_[chunk_index] = nullptr;
+			}
+		}
+	}
+	return object;
+}
+
+template<class OBJECT>
+bool Foundation::ObjectMgr<OBJECT>::DeleteObj(u32 id) {
+	OBJECT * object = RemoveObj(id);
+	if (!object) {
+		return false;
+	} else {
+		delete object;
+		return true;
+	}
+}
+
+template<class OBJECT>
+void Foundation::ObjectMgr<OBJECT>::DeleteAllObjs() {
+	for (auto & it : active_chunk_) {
+		CHUNK * chunk = it;
+		if (chunk) {
+			if (chunk->object_count > 0) {
+				for (u32 i = 0; i < object_count_per_chunk_; ++i) {
+					OBJECT * object = *(chunk->objects + i);
+					if (object) {
+						delete object;
+					}
+				}
+			}
+			if (chunk->objects) {
+				free(chunk->objects);
+			}
+			delete chunk;
+		}
+	}
+	active_chunk_.clear();
+	active_chunk_.reserve(init_chunk_count_);
+	next_chunk_index_ = 0;
+	next_object_index_ = 1;
+	next_object_id_ = 1;
+	while (!free_chunk_.empty()) {
+		CHUNK * chunk = free_chunk_.back();
+		if (chunk->objects) {
+			free(chunk->objects);
+		}
+		delete chunk;
+		free_chunk_.pop_back();
+	}
+}
+
+template<class OBJECT>
+void Foundation::ObjectMgr<OBJECT>::EnumEachObj(void(*EachFunc)(void * object, void * ud), void * ud) {
+	for (auto & it : active_chunk_) {
+		CHUNK * chunk = it;
+		if (chunk && chunk->object_count > 0) {
+			for (u32 i = 0; i < object_count_per_chunk_; ++i) {
+				OBJECT * object = *(chunk->objects + i);
+				if (object) {
+					EachFunc(static_cast<void *>(object), ud);
+				}
+			}
+		}
+	}
+}
+
+#endif
---
+++ TestSuite/ObjectMgrTestSuite.cc
@@ -0,0 +1,129 @@
+#include "gtest/gtest.h"
+#include "ObjectMgr.h"
+
+using namespace Foundation;
+
+class TestObj {
+public:
+	TestObj(int value) : value_(value) {}
+	virtual ~TestObj() {}
+	int GetValue() const { return value_; }
+
+private:
+	int value_;
+};
+
+TEST(ObjectMgrTestSuite, Construct) {
+	ObjectMgr<TestObj> mgr1(5, 8);
+	ObjectMgr<TestObj> mgr2(50, 8);
+	ObjectMgr<TestObj> mgr3(500, 8);
+	ObjectMgr<TestObj> mgr4(5000, 8);
+}
+
+TEST(ObjectMgrTestSuite, Add) {
+	ObjectMgr<TestObj> mgr1(5, 8);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(1)), 1);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(2)), 2);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(3)), 3);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(4)), 4);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(5)), 5);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(6)), 6);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(7)), 7);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(8)), 8);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(9)), 9);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(10)), 10);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(11)), 11);
+	EXPECT_EQ(mgr1.AddNewObj(new TestObj(12)), 12);
+}
+
+TEST(ObjectMgrTestSuite, Add2) {
+	ObjectMgr<TestObj> mgr1(5, 8);
+	EXPECT_EQ(mgr1.AddNewObj(1, new TestObj(1)), 1);
+	EXPECT_EQ(mgr1.AddNewObj(5, new TestObj(2)), 5);
+	EXPECT_EQ(mgr1.AddNewObj(10, new TestObj(3)), 10);
+	EXPECT_EQ(mgr1.AddNewObj(15, new TestObj(4)), 15);
+	EXPECT_EQ(mgr1.AddNewObj(20, new TestObj(5)), 20);
+	EXPECT_EQ(mgr1.AddNewObj(32, new TestObj(6)), 32);
+	EXPECT_EQ(mgr1.AddNewObj(64, new TestObj(7)), 64);
+	EXPECT_EQ(mgr1.AddNewObj(128, new TestObj(8)), 128);
+	EXPECT_EQ(mgr1.AddNewObj(256, new TestObj(9)), 256);
+	EXPECT_EQ(mgr1.AddNewObj(512, new TestObj(10)), 512);
+	EXPECT_EQ(mgr1.AddNewObj(1024, new TestObj(11)), 1024);
+	EXPECT_EQ(mgr1.AddNewObj(2000, new TestObj(12)), 2000);
+}
+
+TEST(ObjectMgrTestSuite, Get) {
+	ObjectMgr<TestObj> mgr1(5, 8);
+	mgr1.AddNewObj(1, new TestObj(1));
+	mgr1.AddNewObj(5, new TestObj(2));
+	mgr1.AddNewObj(10, new TestObj(3));
+	mgr1.AddNewObj(15, new TestObj(4));
+	mgr1.AddNewObj(20, new TestObj(5));
+	mgr1.AddNewObj(32, new TestObj(6));
+	mgr1.AddNewObj(64, new TestObj(7));
+	mgr1.AddNewObj(128, new TestObj(8));
+	mgr1.AddNewObj(256, new TestObj(9));
+	mgr1.AddNewObj(512, new TestObj(10));
+	mgr1.AddNewObj(1024, new TestObj(11));
+	mgr1.AddNewObj(2000, new TestObj(12));
+
+	EXPECT_EQ(mgr1.GetObj(500), nullptr);
+	EXPECT_NE(mgr1.GetObj(1024), nullptr);
+	EXPECT_EQ(mgr1.GetObjCount(), 12);
+}
+
+TEST(ObjectMgrTestSuite, Remove) {
+	ObjectMgr<TestObj> mgr1(5, 8);
+	mgr1.AddNewObj(1, new TestObj(1));
+	mgr1.AddNewObj(5, new TestObj(2));
+	mgr1.AddNewObj(10, new TestObj(3));
+	mgr1.AddNewObj(15, new TestObj(4));
+	mgr1.AddNewObj(20, new TestObj(5));
+	mgr1.AddNewObj(32, new TestObj(6));
+	mgr1.AddNewObj(64, new TestObj(7));
+	mgr1.AddNewObj(128, new TestObj(8));
+	mgr1.AddNewObj(256, new TestObj(9));
+	mgr1.AddNewObj(512, new TestObj(10));
+	mgr1.AddNewObj(1024, new TestObj(11));
+	mgr1.AddNewObj(2000, new TestObj(12));
+
+	TestObj * obj = mgr1.RemoveObj(128);
+	EXPECT_EQ(obj->GetValue(), 8);
+	delete obj;
+	obj = mgr1.RemoveObj(500);
+	EXPECT_EQ(obj, nullptr);
+
+	EXPECT_EQ(mgr1.DeleteObj(200), false);
+	EXPECT_EQ(mgr1.DeleteObj(256), true);
+
+	EXPECT_EQ(mgr1.GetObj(256), nullptr);
+	EXPECT_EQ(mgr1.DeleteObj(256), false);
+
+	mgr1.AddNewObj(200, new TestObj(13));
+	mgr1.AddNewObj(256, new TestObj(14));
+}
+
+void Func(void * object, void * ud) {
+	int * sum = static_cast<int *>(ud);
+	*sum += static_cast<TestObj *>(object)->GetValue();
+}
+
+TEST(ObjectMgrTestSuite, Each) {
+	ObjectMgr<TestObj> mgr1(5, 8);
+	mgr1.AddNewObj(1, new TestObj(1));
+	mgr1.AddNewObj(5, new TestObj(2));
+	mgr1.AddNewObj(10, new TestObj(3));
+	mgr1.AddNewObj(15, new TestObj(4));
+	mgr1.AddNewObj(20, new TestObj(5));
+	mgr1.AddNewObj(32, new TestObj(6));
+	mgr1.AddNewObj(64, new TestObj(7));
+	mgr1.AddNewObj(128, new TestObj(8));
+	mgr1.AddNewObj(256, new TestObj(9));
+	mgr1.AddNewObj(512, new TestObj(10));
+	mgr1.AddNewObj(1024, new TestObj(11));
+	mgr1.AddNewObj(2000, new TestObj(12));
+
+	int sum = 0;
+	mgr1.EnumEachObj(Func, static_cast<void *>(&sum));
+	EXPECT_EQ(sum, 78);
+}
