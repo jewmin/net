1ee1f9f22a75b611a4161f03978f42cf7ae40b54
jim chen <chenzemingczm@163.com>
2020-04-11 16:41:03 +0800

    修改编译脚本

--- build.bat
+++ build.bat
@@ -1,3 +1,37 @@
+@echo off
+
+cd %~dp0
+
+set config=Debug
+
+:next-arg
+if "%1"=="" goto args-done
+if /i "%1"=="debug"        set config=Debug&goto arg-ok
+if /i "%1"=="release"      set config=Release&goto arg-ok
+:arg-ok
+shift
+goto next-arg
+:args-done
+ 
+set MSBuild="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\"
+call %MSBuild%VsMSBuildCmd.bat
+
+if "%config%"=="Release" goto build-release
+
 mkdir build\debug
 cd build\debug
 cmake ../.. -G "Visual Studio 15 2017 Win64" -DRELEASE=OFF
+
+goto build-run
+
+:build-release
+
+mkdir build\release
+cd build\release
+cmake ../.. -G "Visual Studio 15 2017 Win64" -DRELEASE=ON
+
+:build-run
+msbuild libnet.sln /t:Build /p:Configuration=%config% /p:Platform=x64 /clp:NoSummary;NoItemAndPropertyList;Verbosity=minimal /nologo
+cd ../..
+
+:exit
